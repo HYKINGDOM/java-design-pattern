52e3833335ccb024ceace77dbb70077c60945915
罗文强 <luo_wenqiang@qq.com>
2019-09-12 22:04:29 +0800

    修改配置中心，增加Git仓库配置和本地仓库配置兼容方式。使用本地测试的时候，请在运行程序增加spring.profiles.active=native,dev两个参数！使用Git则需要修改application.yaml文件中的Git仓库地址、用户名和密码。

--- portal/config-center/src/main/java/org/fkjava/config/ConfigApp.java
+++ portal/config-center/src/main/java/org/fkjava/config/ConfigApp.java
@@ -2,15 +2,12 @@ package org.fkjava.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.config.ConfigFileApplicationListener;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 // 激活配置中心服务器
 @EnableConfigServer
 public class ConfigApp {
-
-    ConfigFileApplicationListener d;
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigApp.class, args);
